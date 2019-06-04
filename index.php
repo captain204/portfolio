512545982ddcca667b6385c06d29d316a488d482
Nurudeen Akindele <nurudeenakindele8@gmail.com>
2019-06-04 12:57:19 +0100

    bootstrap template

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/portfolio.css">
+	<script src="js/jquery.js"></script>
+	<link rel="stylesheet" href="js/bootstrap.min.js">
+	<title>Portfolio</title>
+</head>
+<body data-spy="scroll" data-target=".navbar">
+	<nav class="nav navbar-inverse navbar-fixed-bottom">
+		<ul class="nav navbar-nav">
+			<li><a href="#home">Home</a></li>
+			<li><a href="#myworks">My works </a></li>
+			<li><a href="#about">About me</a></li>
+			<li><a href="#contact">Contact me</a></li>
+		</ul>
+	</nav>
+	<section id="home" style="background: url(images/bg-callout.jpg); background-size: 100% 100%" class="cl_white text-center">
+		<h1>My portfolio</h1>
+		<p class="lead">For your well built custom web solutions contact me today</p>
+		<button class="btn btn-default">Contact me</button>
+	</section>
+
+	<section id="myworks" class="container">
+		<div class="page-heading">
+			<h1 class="text-center">My works</h1>
+		</div>
+			<img src="images/laptop-2557574__340.jpg" class="img-thumbnail">
+			<img src="images/laptop-3190194__340.jpg" class="img-thumbnail">			
+			<img src="images/indoors-3211177__340.jpg" class="img-thumbnail">
+			<img src="images/laptop-3190194__340.jpg" class="img-thumbnail">
+			<img src="images/laptop-2557574__340.jpg" class="img-thumbnail">
+			<img src="images/indoors-3211177__340.jpg" class="img-thumbnail">	
+	</section>
+
+	<section id="about" class="container-fluid">
+			<h4 class="text-center">About me(Random text here)</h4>
+		<div class="col-text">
+			Seasoned web developer with knowledge of modern development tools and methodologies.
+			Give me a try and I will try not over deliver beyond your expectations.The modern leader, whether in the public or private sector is a knowledge worker. In essence, to be an effective leader you need to be an expert in various functional areas of any organization you are overseeing. One of the major challenges of 21st-century leadership is the ability of leaders to make informed decisions that have the capacity to change the fortune of the organizations they are overseeing. In fact, the success or failure of any organization depends on the ability of its leaders to make a good decision. The most important ingredient in decision making is accurate and timely information. 
+			Information on its own has no value to a leader if it’s inaccurate, untimely or at best incomplete. No matter how talented or skilled a leader is there is no way such a leader can make even simple routine decisions with information that have any of the above-listed qualities.  So how does information communication technology come into the entire gist or what do they do in the entire process of decision-making and leadership?
+		</div><br>
+		<div class="row">
+			<div class="col-md-6">				
+				<div class="panel panel-primary">
+					<div class="panel panel-heading">Web development</div>
+					<div class="panel panel-body">
+					<p class="lead text-center">
+					<img src="images/laptop-3190194__340.jpg" class="img-circle center-block">
+					I build web apps that will transform your buisiness!</p>
+					</div>
+				<div class="panel panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-6">				
+				<div class="panel panel-primary">
+					<div class="panel panel-heading">Web development</div>
+					<div class="panel panel-body">
+					<p class="lead text-center">
+					<img src="images/laptop-3190194__340.jpg" class="img-circle center-block">
+					I build web apps that will transform your buisiness!</p>
+					</div>
+				<div class="panel panel-footer"></div>
+				</div>
+			</div>
+		 </div>
+	</section>
+
+
+	<section id="contact" class="" style="background-color:silver;">
+		<div class="page-header"><h2 class="text-center">Contact me</h2></div>
+		<form class="col-md-5 col-md-offset-4">
+			<div class="form-group">
+				<input type="text" placeholder="enter your name" class="form-control">
+			</div>
+			<div class="form-group">
+				<input type="emai" placeholder="enter your email" class="form-control">
+			</div>
+			<div class="form-group">
+				<input type="text" placeholder="enter your subject" class="form-control">
+			</div>
+			<div class="form-group">
+				<textarea rows="2" class="form-control">Message</textarea>
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary btn-block">submit</button>
+			</div>
+		</form>
+
+	</section>
+	
+</body>
+</html>
